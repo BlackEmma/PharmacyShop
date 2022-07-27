3ad75a747542e593733471b10e097bc710d53cac
Vitaliy <vetal9305@gmail.com>
2022-07-27 19:35:40 +0300

    fix

--- views/Home.jsx
+++ views/Home.jsx
@@ -1,18 +1,19 @@
 const React = require('react');
 const Layout = require('./Layout');
+const { Item } = require('./Item');
 
-module.exports = function Home() {
+module.exports = function Home({ arrDrugs }) {
   return (
     <Layout>
 
       <section className="itemWeek">
         <h1>Товары недели</h1>
-        {arr.map()}
+        {arrDrugs.map((el) => <Item drug={el} />)}
       </section>
 
       <section className="itemList">
         <h1>Лекарства в наличии</h1>
-        {arr.map()}
+        {arrDrugs.map((el) => <Item drug={el} />)}
       </section>
 
     </Layout>
