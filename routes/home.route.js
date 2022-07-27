0fcaadf63383747e3647ae84c677e4cf03f8262b
Ekaterina Ekimova <psich.kawai@gmail.com>
2022-07-27 19:43:52 +0300

    added home route

--- routes/home.route.js
+++ routes/home.route.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const Home = require('../views/Home');
+const { Drug } = require('../db/models');
 
 router.route('/')
-  .get((req, res) => {
-    res.renderComponent(Home, {});
+  .get(async (req, res) => {
+    const arrDrugs = await Drug.findAll();
+    res.renderComponent(Home, { arrDrugs });
   });
 
 module.exports = router;
