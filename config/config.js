f3a0c141fc46e130d398d065b39aead8ae66ffc1
Ekaterina Ekimova <psich.kawai@gmail.com>
2022-07-27 16:37:38 +0300

    added session to config

--- config/config.js
+++ config/config.js
@@ -1,9 +1,24 @@
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 const express = require('express');
 const ssr = require('../middleware/ssr');
 
 const config = (app) => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
+  app.use(session(
+    {
+      store: new FileStore(), // создаем хранилище для сессии
+      name: 'user_sid', // Имя куки для хранения id сессии. По умолчанию - connect.sid
+      secret: process.env.SESSION_SECRET ?? 'test', // Секретное слово для шифрования, может быть любым
+      resave: false, // Пересохранять ли куку при каждом запросе
+      saveUninitialized: false, // Создавать ли сессию без инициализации ключей в req.session
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 12, // Срок истечения годности куки в миллисекундах
+        httpOnly: true, // Серверная установка и удаление куки, по умолчанию true
+      },
+    },
+  ));
   app.use(express.static('public'));
   app.use(ssr);
 };
