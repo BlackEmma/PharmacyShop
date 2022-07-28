756df6c32a3afe48a76b8a364c3cfc974b3dc893
Vitaliy <vetal9305@gmail.com>
2022-07-28 15:42:42 +0300

    add logic user login

--- views/Item.jsx
+++ views/Item.jsx
@@ -1,16 +1,31 @@
 const React = require('react');
 
-module.exports = function Item({ drug }) {
+module.exports = function Item({ drug, user }) {
   return (
-    <div className="card" style={{ width: '18rem' }}>
-      <img src={`${drug.picture}`} className="card-img-top" alt="Здесь должна быть фоторафия лекарства" />
-      <div className="card-body">
-        <h5 className="card-title">{drug.title}</h5>
-        <p className="card-text">{drug.description}</p>
-        <p className="card-text">{drug.currentPrice}</p>
-        <p className="card-text">{drug.priceOff}</p>
-        <a href="#" className="btn btn-primary">Купить</a>
-      </div>
-    </div>
+    <section>
+      {user ? (
+        <div className="card" style={{ width: '18rem' }}>
+          <img src={`${drug.picture}`} className="card-img-top" alt="Здесь должна быть фоторафия лекарства" />
+          <div className="card-body">
+            <h5 className="card-title">{drug.title}</h5>
+            <p className="card-text">{drug.description}</p>
+            <p className="card-text">{drug.currentPrice}</p>
+            <p className="card-text">{drug.priceOff}</p>
+            <a href="#" className="btn btn-primary">Купить</a>
+          </div>
+        </div>
+      ) : (
+        <div className="card" style={{ width: '18rem' }}>
+          <img src={`${drug.picture}`} className="card-img-top" alt="Здесь должна быть фоторафия лекарства" />
+          <div className="card-body">
+            <h5 className="card-title">{drug.title}</h5>
+            <p className="card-text">{drug.description}</p>
+            <p className="card-text">{drug.currentPrice}</p>
+            <p className="card-text">{drug.priceOff}</p>
+            <a href="/log" className="btn btn-primary">Купить</a>
+          </div>
+        </div>
+      )}
+    </section>
   );
 };
