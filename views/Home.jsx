f557cfa2e8f4d3e40c65658cf30f718560191002
Vitaliy <vetal9305@gmail.com>
2022-07-29 12:15:30 +0300

    added style fix free items

--- views/Home.jsx
+++ views/Home.jsx
@@ -1,18 +1,19 @@
 const React = require('react');
 const Layout = require('./Layout');
 const Item = require('./Item');
+const FreeItem = require('./FreeItem');
 
 module.exports = function Home({ arrDrugs, count, userId }) {
   return (
     <Layout userId={userId} count={count}>
       <div className="container">
         <section className="itemWeek">
-          <h1>Товары недели</h1>
-          {arrDrugs.map((el) => <Item key={el.id} drug={el} userId={userId} />)}
+          <h3>Товары недели</h3>
+          {arrDrugs.map((el) => <FreeItem key={el.id} drug={el} />)}
         </section>
         <section className="itemList">
-          <h1>Лекарства в наличии</h1>
-          {arrDrugs.map((el) => <Item key={el.id} drug={el} userId={userId} />)}
+          <h3>Лекарства в наличии</h3>
+          {arrDrugs.map((el) => <Item key={el.id} drug={el} />)}
         </section>
       </div>
     </Layout>
