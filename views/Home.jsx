4ab362428691e6e2daab7c004aca591db0912413
Vitaliy <vetal9305@gmail.com>
2022-07-29 15:41:09 +0300

    fix slice

--- views/Home.jsx
+++ views/Home.jsx
@@ -7,16 +7,13 @@ module.exports = function Home({ arrDrugs, count, userId }) {
   return (
     <Layout userId={userId} count={count}>
       <div className="container">
-      <h3>Товары недели</h3>
+        <h3>Товары недели</h3>
         <section className="itemWeek">
-          
           {arrDrugs.slice(0, 3).map((el) => <FreeItem key={el.id} drug={el} />)}
         </section>
-
-
         <h3>Все лекарства</h3>
         <section className="itemList">
-          {arrDrugs.map((el) => <Item key={el.id} drug={el} />)}
+          {arrDrugs.slice(3).map((el) => <Item key={el.id} drug={el} />)}
         </section>
       </div>
     </Layout>
